cfc2279573975270afdbf565116aaf532cff6f4f
hayitmurod707 <hayitmurod707@gmail.com>
2023-01-14 20:12:59 +0500

    updated middleware and getOptions

--- request/getOptions.js
+++ request/getOptions.js
@@ -1,13 +1,16 @@
 const getOptions = auth => {
-	const token = localStorage.getItem('token');
+	const token = localStorage.getItem("token");
 	const headers = auth ? { Authorization: `Bearer ${token}` } : {};
-	const baseURL = process.env.NODE_ENV === 'development'? process.env.REACT_APP_TEST_API_URL : process.env.REACT_APP_API_URL;
-	const i18nextLng = localStorage.getItem('i18nextLng');
-	const lang = i18nextLng ? i18nextLng : 'uz';
+	const baseURL =
+		process.env.NODE_ENV === "development"
+			? process.env.REACT_APP_TEST_API_URL
+			: process.env.REACT_APP_API_URL;
+	const i18nextLng = localStorage.getItem("i18nextLng");
+	const lang = i18nextLng ? i18nextLng : "uz";
 	return {
 		baseURL,
 		headers,
 		params: { lang },
 	};
 };
-export default getOptions
+export default getOptions;
--- request/middleware.js
+++ request/middleware.js
@@ -1,18 +1,12 @@
-import toast from 'react-toastify';
+import toast from "react-toastify";
 const middleware = error => {
-  if (typeof error?.toJSON === 'function') {
-    const e = error?.toJSON();
-    if (e?.message === 'Network Error') {
-      const message =
-        localStorage.getItem('i18nextLng') === 'ru'
-          ? 'Сетевая ошибка'
-          : 'Tarmoq xatoligi';
-      toast.warning(message);
-    }
-    if (process.env.NODE_ENV === 'development') {
-      console.log(e);
-    }
-  }
-  return Promise.reject(error);
-}
-export default middleware
+	if (typeof error?.toJSON === "function") {
+		const e = error?.toJSON();
+		toast.warning(e?.message);
+		if (process.env.NODE_ENV === "development") {
+			console.log(e);
+		}
+	}
+	return Promise.reject(error);
+};
+export default middleware;
